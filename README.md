a137319d3cb3a452a1232ff7c0cd0d61c0b21dd8
Joas Kramer <joaskramer@outlook.com>
2021-03-10 13:36:27 +0100

    Add mongoose to query MongoDB, dummy data and GET methods to retrieve data

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # RESTful Web Services with Node.js and Express
 
 This repository is based on the Pluralsight course with the same name as the title by Jonathan Mills. It is used to catch back up on Node.js, since it's been a while since I last used it in a project.
+
+## Importing book data
+
+Inside this project is a file named bookJson.js, which holds the required data for this project to run. You can import this data into MongoDB if you have mongo running. Run the following command to start the import:
+
+```bash
+mongo bookAPI booksJson.js
+```
--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost:27017/bookAPI', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
 bookRouter.route('/books').get((req, res) => {
-  const response = { hello: 'This is my API' };
-  res.json(response);
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+
+  Book.find(query, (err, books) => {
+    if (err) res.send(err);
+    res.json(books);
+  });
 });
+
+bookRouter.route('/books/:bookId').get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) res.send(err);
+    res.json(book);
+  });
+});
+
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
