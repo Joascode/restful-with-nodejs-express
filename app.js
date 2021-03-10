f2c5c6026af5e99811250eb5fb8cbfca77b53bfa
Joas Kramer <joaskramer@outlook.com>
2021-03-10 14:17:46 +0100

    Add POST action for a book and cleaned up the code a little bit

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Book = require('./models/bookModel');
+const { urlencoded, json } = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost:27017/bookAPI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const bookRouter = express.Router();
+const bookRouter = require('./routes/bookRouter')(Book);
 const port = process.env.PORT || 3000;
 
-bookRouter.route('/books').get((req, res) => {
-  const query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-
-  Book.find(query, (err, books) => {
-    if (err) res.send(err);
-    res.json(books);
-  });
-});
-
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) res.send(err);
-    res.json(book);
-  });
-});
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.use('/api', bookRouter);
 
