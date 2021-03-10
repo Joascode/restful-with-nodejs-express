0004b7d7360512a59a74b2986e822539cafcf698
Joas Kramer <joaskramer@outlook.com>
2021-03-10 19:50:35 +0100

    Add PUT, PATCH and DELETE actions to books endpoint

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Book = require('./models/bookModel');
 const { urlencoded, json } = require('body-parser');
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost:27017/bookAPI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const bookRouter = require('./routes/bookRouter')(Book);
 const port = process.env.PORT || 3000;
 
 app.use(urlencoded({ extended: true }));
